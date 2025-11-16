cc8852e0cbc0538c3a26156ddafb32f84af5440c
Your Name <you@example.com>
2025-11-16 21:26:27 +0600

    OK

--- playwright_stealth_plugin/evasions/python/_utils.py
+++ playwright_stealth_plugin/evasions/python/_utils.py
@@ -2,13 +2,10 @@ import os
 import sys
 import logging
 
-init_scripts = []
+# onPageCreated beforeLaunch beforeConnect getDefaults
 logger = logging.getLogger("playwright_stealth_plugin")
 
 
 def read_script(script_path: str) -> str:
     with open(script_path, "r") as script:
         return script.read()
-
-
-init_scripts.append(read_script(f"../javascript/_utils.js"))
