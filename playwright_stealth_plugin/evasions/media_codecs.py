2db06578277ba5fbbc7580d08d2bead846839662
Your Name <you@example.com>
2025-11-16 12:08:32 +0600

    OK

--- playwright_stealth_plugin/evasions/media_codecs.py
+++ playwright_stealth_plugin/evasions/media_codecs.py
@@ -1,4 +1,5 @@
 import playwright.async_api
 
-async def run(playwright: playwright.async_api.Playwright):
-    pass
+async def run(playwright: playwright.async_api.Playwright) -> bool:
+    # write code here
+    return True
