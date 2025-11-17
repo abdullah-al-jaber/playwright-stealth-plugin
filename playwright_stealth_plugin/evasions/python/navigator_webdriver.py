9734c685b921c59a48b3296f26aaa8c0d00cd29b
Your Name <you@example.com>
2025-11-17 14:21:11 +0600

    OK

--- playwright_stealth_plugin/evasions/python/navigator_webdriver.py
+++ playwright_stealth_plugin/evasions/python/navigator_webdriver.py
@@ -5,3 +5,16 @@ async def run(playwright: playwright.async_api.Playwright):
     script = _utils.read_script(f"../javascript/{__name__.split(".")[-1]}.js")
     _utils.scripts.append(script)
     _utils.logger.info(f"RUN: {__name__}")
+
+function beforeLaunch(options) {
+  // If disable-blink-features is already passed, append the AutomationControlled switch
+  const idx = options.args.findIndex((arg) =>
+    arg.startsWith("--disable-blink-features=")
+  );
+  if (idx !== -1) {
+    const arg = options.args[idx];
+    options.args[idx] = `${arg},AutomationControlled`;
+  } else {
+    options.args.push("--disable-blink-features=AutomationControlled");
+  }
+}
--- playwright_stealth_plugin/evasions/python/window_outer_dimension.py
+++ playwright_stealth_plugin/evasions/python/window_outer_dimension.py
@@ -5,3 +5,12 @@ async def run(playwright: playwright.async_api.Playwright):
     script = _utils.read_script(f"../javascript/{__name__.split(".")[-1]}.js")
     _utils.scripts.append(script)
     _utils.logger.info(f"RUN: {__name__}")
+
+async beforeLaunch(options) {
+    // Have viewport match window size, unless specified by user
+    // https://github.com/GoogleChrome/puppeteer/issues/3688
+    if (!('defaultViewport' in options)) {
+      options.defaultViewport = null
+    }
+    return options
+  }
