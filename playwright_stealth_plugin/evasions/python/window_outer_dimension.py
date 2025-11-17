031f3b498c6a009bbea380ad69e44ff4db47db66
Your Name <you@example.com>
2025-11-17 21:13:23 +0600

    OK

--- playwright_stealth_plugin/evasions/python/window_outer_dimension.py
+++ playwright_stealth_plugin/evasions/python/window_outer_dimension.py
@@ -4,14 +4,5 @@ import playwright_stealth_plugin.evasions.python._utils as _utils
 async def run():
     script = _utils.read_script(f"../javascript/{__name__.split(".")[-1]}.js")
     _utils.scripts.append(script)
+    _utils.options["context"]["viewport"] = None
     _utils.logger.info(f"RUN: {__name__}")
-
-
-# async beforeLaunch(options) {
-#     // Have viewport match window size, unless specified by user
-#     // https://github.com/GoogleChrome/puppeteer/issues/3688
-#     if (!('defaultViewport' in options)) {
-#       options.defaultViewport = null
-#     }
-#     return options
-#   }
