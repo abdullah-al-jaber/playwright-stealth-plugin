7587320af47bb27e407d27d364d45c342d175b05
Your Name <you@example.com>
2025-11-20 21:48:45 +0600

    OK

--- playwright_stealth_plugin/_python/config.py
+++ playwright_stealth_plugin/_python/config.py
@@ -1,27 +1,7 @@
 import typing
 
 script_folder = "_javascript"
-script_names: typing.List[str] = [
-    "utils.js",
-    "chrome.app.js",
-    "chrome.csi.js",
-    "chrome.hairline.js",
-    "chrome.load.times.js",
-    "chrome.runtime.js",
-    "generate.magic.arrays.js",
-    "iframe.contentWindow.js",
-    "media.codecs.js",
-    "navigator.hardwareConcurrency.js",
-    "navigator.languages.js",
-    "navigator.permissions.js",
-    "navigator.platform.js",
-    "navigator.plugins.js",
-    "navigator.userAgent.js",
-    "navigator.vendor.js",
-    "navigator.webdriver.js",
-    "webgl.vendor.js",
-    "window.outerdimensions.js",
-]
+script_names: typing.List[str] = []
 
 browser_options: typing.Dict[str, typing.Any] = {}
 context_options: typing.Dict[str, typing.Any] = {}
