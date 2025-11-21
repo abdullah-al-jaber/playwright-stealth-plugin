aa470c48471017b50bc4625f10993fd201357a2f
Your Name <you@example.com>
2025-11-21 15:34:41 +0600

    OK

--- playwright_stealth_plugin/__init__.py
+++ playwright_stealth_plugin/__init__.py
@@ -1,6 +1,4 @@
-import playwright_stealth_plugin._python.async_api as async_api
-import playwright_stealth_plugin._python.sync_api as sync_api
+from ._python.async_api import apply as async_apply
+from ._python.sync_api import apply as sync_apply
 
-async_apply = async_api.apply
-sync_apply = sync_api.apply
 __all__ = ["async_apply", "sync_apply"]
