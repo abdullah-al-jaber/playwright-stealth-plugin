0075f5cb0e32ad290f9988e34a74c0e57c299d84
Your Name <you@example.com>
2025-11-25 12:00:33 +0600

    OK

--- playwright_stealth_plugin/_python/config.py
+++ playwright_stealth_plugin/_python/config.py
@@ -5,7 +5,14 @@ import typing
 sys.__file__ = os.path.abspath(os.path.join(__file__, ".."))
 
 script_folder = "_javascript"
-script_names: typing.List[str] = []
+script_names: typing.List[str] = ["utils.js"]
 
-browser_options: typing.Dict[str, typing.Any] = {}
-context_options: typing.Dict[str, typing.Any] = {}
+browser_options: typing.Dict[str, typing.Any] = {"args": ["--disable-blink-features=AutomationControlled"]}
+context_options: typing.Dict[str, typing.Any] = {
+    "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.7390.37 Safari/537.36",
+    "viewport": {"width": 1280, "height": 720},
+    "device_scale_factor": 1,
+    "is_mobile": False,
+    "has_touch": False,
+    "default_browser_type": "chromium",
+}
