77cf03e78d75bd63270e445eca39d41e7f229305
Your Name <you@example.com>
2026-03-27 19:58:43 +0600

    OK

--- playwright_stealth_plugin/_javascript/cdp.js
+++ playwright_stealth_plugin/_javascript/cdp.js
@@ -1,12 +1,12 @@
 const cdp = () => {
-    Object.defineProperty(Error, "stackTraceLimit", {
-        configurable: false,
-        writable: false,
-        value: 0,
-    });
-    Object.defineProperty(Error, "prepareStackTrace", {
-        value: () => { },
-    });
+  Object.defineProperty(Error, "stackTraceLimit", {
+    configurable: false,
+    writable: false,
+    value: 0,
+  });
+  Object.defineProperty(Error, "prepareStackTrace", {
+    value: () => {},
+  });
 };
 cdp();
 window.worker_scripts.push(`(${cdp.toString()})();`);
--- playwright_stealth_plugin/_typescript/_utils.ts
+++ playwright_stealth_plugin/_typescript/_utils.ts
@@ -1,26 +1,28 @@
 const worker_scripts = [];
 
 const sync_fetch = (url: string): string => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, false);
-    xhr.send();
-    if (xhr.status !== 200) throw new Error("Response isn't valid !");
-    return xhr.responseText;
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, false);
+  xhr.send();
+  if (xhr.status !== 200) throw new Error("Response isn't valid !");
+  return xhr.responseText;
 };
 
 const original_Worker = window.Worker;
 const custom_Worker = (scriptURL: string | URL, options?: WorkerOptions): Worker => {
-    const url = typeof scriptURL == "string" ? scriptURL : scriptURL.href;
-    const code = sync_fetch(url);
-    const blob = new Blob([...worker_scripts, code], { type: "application/javascript" });
-    return new original_Worker(URL.createObjectURL(blob));
+  const url = typeof scriptURL == "string" ? scriptURL : scriptURL.href;
+  const code = sync_fetch(url);
+  const blob = new Blob([...worker_scripts, code], {
+    type: "application/javascript",
+  });
+  return new original_Worker(URL.createObjectURL(blob));
 };
 window.Worker = custom_Worker as unknown as typeof Worker;
 
 declare global {
-    interface Window {
-        worker_scripts: string[];
-    }
+  interface Window {
+    worker_scripts: string[];
+  }
 }
 export {};
 
